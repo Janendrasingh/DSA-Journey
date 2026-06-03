019e7dad0e21887e99619c5558e36f35a6898112
JANENDRA SINGH <singhjanendra120202@gmail.com>
2026-06-03 11:26:20 +0530

    Add Selection Sort implementation

---
+++ 1.1 Selection_Sorting.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void selection_sorting(int n, int arr[]){
+	for(int i=0; i<n-1; i++){
+		int minItem=i;
+			for(int j=i+1; j<n; j++){
+				if(arr[j]<arr[minItem]){
+					minItem = j;
+				}
+			}
+			swap(arr[i], arr[minItem]);
+	}
+}
+
+int main(){
+	int n;
+	cout<<"Enter the Size of array: ";
+	cin>>n;
+	int arr[n];
+	
+	cout<<"Enter the elements"<<endl;
+	for(int i=0; i<n; i++){
+		cin>>arr[i];
+	}
+	
+	cout<<"Before Sorting: [";
+	for(int i=0; i<n; i++){
+		cout<<"\""<<arr[i]<<"\" ";
+	}
+	cout<<"]"<<endl;
+	
+	selection_sorting(n, arr);
+	
+	cout<<"After Sorting:  [";
+	for(int i=0; i<n; i++){
+		cout<<"\""<<arr[i]<<"\" ";
+	}
+	
+	cout<<"]";
+}
