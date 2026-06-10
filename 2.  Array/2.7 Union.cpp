c4b80ba9a0f6df22f0f9591868709263aa898f30
JANENDRA SINGH <singhjanendra120202@gmail.com>
2026-06-10 23:56:06 +0530

    Added union of two sorted array file

--- 2.  Array/2.7 Union.cpp
+++ 2.  Array/2.7 Union.cpp
@@ -4,5 +4,23 @@
 using namespace std;
 
 int main(){
+	int n, m;
+	cout<<"Enter the size of 1st Array: ";
+	cin>>n;
+	cout<<"Enter the size of 2nd Array: ";
+	cin>>m;
+	vector<int> a1(n);
+	vector<int> a2(m);
 	
+	cout<<"Enter the elements of 1st Array"<<endl;
+	for(int i=0; i<n; i++){
+		cin>>a1[i];
+	}
+	
+	cout<<"Enter the elements of 2nd Array"<<endl;
+	for(int i=0; i<m; i++){
+		cin>>a2[i];
+	}
+	
+	cout<<"Union of these two Array are: ";
 }
