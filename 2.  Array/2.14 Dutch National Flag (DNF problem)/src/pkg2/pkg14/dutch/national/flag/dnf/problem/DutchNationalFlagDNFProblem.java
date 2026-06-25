f536f7ec39e83f18d4a444e924487202174dee88
JANENDRA SINGH <singhjanendra120202@gmail.com>
2026-06-25 11:30:34 +0530

    added better solution

--- 2.  Array/2.14 Dutch National Flag (DNF problem)/src/pkg2/pkg14/dutch/national/flag/dnf/problem/DutchNationalFlagDNFProblem.java
+++ 2.  Array/2.14 Dutch National Flag (DNF problem)/src/pkg2/pkg14/dutch/national/flag/dnf/problem/DutchNationalFlagDNFProblem.java
@@ -1,31 +1,51 @@
-
 // Sorting 0's, 1's, 2's Solving by Dutch Flag Algo
-
-
 package pkg2.pkg14.dutch.national.flag.dnf.problem;
+
 import java.util.*;
 
 public class DutchNationalFlagDNFProblem {
 
     public static void main(String[] args) {
-        // BETTER SOLUTION
-         Scanner sc = new Scanner(System.in);
 
-         System.out.println("Enter the size of array: ");
-         int n = sc.nextInt();
-         int[] arr = new int[n];
-         
-         System.out.println("Enter the Elements");
-         for(int i=0; i<n; i++){
-             arr[i] = sc.nextInt();
-         }
-         
-         
-         
-         System.out.println("Display");
-         for(int i=0; i<n; i++){
-             System.out.println(arr[i]);
-         }
+        Scanner sc = new Scanner(System.in);
+
+        int count0 = 0, count1 = 0, count2 = 0;
+
+        System.out.println("Enter the size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter the Elements");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // BETTER SOLUTION
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) {
+                count0++;
+            }
+            else if (arr[i] == 1) {
+                count1++;
+            } else {
+                count2++;
+            }
+        }
+
+        for (int i = 0; i < count0; i++) {
+            arr[i]=0;
+        }
+        for (int j = count0 ; j < count0+count1; j++) {
+            arr[j] = 1;
+        }
+        for (int k = count0+count1; k < n; k++) {
+            System.out.println(arr[k] = 2);
+        }
+
+        System.out.println("Display");
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
     }
-    
+
 }
