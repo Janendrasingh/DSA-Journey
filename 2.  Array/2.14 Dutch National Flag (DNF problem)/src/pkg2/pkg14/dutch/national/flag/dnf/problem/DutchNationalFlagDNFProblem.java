b1aad72416d46bb93641714dec74ba417b527c21
JANENDRA SINGH <singhjanendra120202@gmail.com>
2026-06-25 13:12:13 +0530

    added optimal Solution

--- 2.  Array/2.14 Dutch National Flag (DNF problem)/src/pkg2/pkg14/dutch/national/flag/dnf/problem/DutchNationalFlagDNFProblem.java
+++ 2.  Array/2.14 Dutch National Flag (DNF problem)/src/pkg2/pkg14/dutch/national/flag/dnf/problem/DutchNationalFlagDNFProblem.java
@@ -5,24 +5,19 @@ import java.util.*;
 
 public class DutchNationalFlagDNFProblem {
 
-<<<<<<< HEAD
     public static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
+        
     }
 
-=======
->>>>>>> f536f7ec39e83f18d4a444e924487202174dee88
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-<<<<<<< HEAD
-=======
         int count0 = 0, count1 = 0, count2 = 0;
 
->>>>>>> f536f7ec39e83f18d4a444e924487202174dee88
         System.out.println("Enter the size of array: ");
         int n = sc.nextInt();
         int[] arr = new int[n];
@@ -33,7 +28,6 @@ public class DutchNationalFlagDNFProblem {
         }
 
         // BETTER SOLUTION
-<<<<<<< HEAD
 //        int count0 = 0, count1 = 0, count2 = 0;
 //        for (int i = 0; i < n; i++) {
 //            if (arr[i] == 0) {
@@ -68,33 +62,31 @@ public class DutchNationalFlagDNFProblem {
                 swap(arr, mid, high);
                 high--;
             }
-=======
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == 0) {
-                count0++;
-            }
-            else if (arr[i] == 1) {
-                count1++;
-            } else {
-                count2++;
-            }
-        }
 
-        for (int i = 0; i < count0; i++) {
-            arr[i]=0;
-        }
-        for (int j = count0 ; j < count0+count1; j++) {
-            arr[j] = 1;
-        }
-        for (int k = count0+count1; k < n; k++) {
-            System.out.println(arr[k] = 2);
->>>>>>> f536f7ec39e83f18d4a444e924487202174dee88
-        }
+            for (int i = 0; i < n; i++) {
+                if (arr[i] == 0) {
+                    count0++;
+                } else if (arr[i] == 1) {
+                    count1++;
+                } else {
+                    count2++;
+                }
+            }
 
-        System.out.println("Display");
-        for (int i = 0; i < n; i++) {
-            System.out.println(arr[i]);
+            for (int i = 0; i < count0; i++) {
+                arr[i] = 0;
+            }
+            for (int j = count0; j < count0 + count1; j++) {
+                arr[j] = 1;
+            }
+            for (int k = count0 + count1; k < n; k++) {
+                System.out.println(arr[k] = 2);
+            }
+
+            System.out.println("Display");
+            for (int i = 0; i < n; i++) {
+                System.out.println(arr[i]);
+            }
         }
     }
-
 }
