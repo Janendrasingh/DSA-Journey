807c2fdfc42c9f74c639c5418d794ee99bf275e8
JANENDRA SINGH <singhjanendra120202@gmail.com>
2026-06-27 11:10:24 +0530

    added Vowel or Consonant using Switch Case

---
+++ Problem List/Question 6 (Day 2).cpp
@@ -0,0 +1,35 @@
+
+//Vowel or Consonant using Switch Case
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+
+    cout << "Enter an alphabet: ";
+    cin >> ch;
+
+    // Convert uppercase letters to lowercase
+    if (ch >= 'A' && ch <= 'Z') {
+        ch = ch + 32;
+    }
+
+    switch (ch) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            cout << "The character is a Vowel.";
+            break;
+
+        default:
+            if (ch >= 'a' && ch <= 'z')
+                cout << "The character is a Consonant.";
+            else
+                cout << "Invalid input! Please enter an alphabet.";
+    }
+
+    return 0;
+}
